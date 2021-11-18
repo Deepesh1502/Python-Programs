a438bdc6d2ddd37c4299a7d01877559c7b2ae72e
Deepesh Garg <63232108+Deepesh1502@users.noreply.github.com>
2021-11-18 12:26:06 +0530

    Create Arithmetic.py

---
+++ Operators/Arithmetic.py
@@ -0,0 +1,8 @@
+x=int(input("Enter a value: "))
+y=int(input("Enter a value: "))
+
+#Python supports operator in the print function
+print ("Addition of",x,"&",y,"is:",x+y)
+print ("Subtraction of",x,"&",y,"is:",x-y)
+print ("Multiplication of",x,"&",y,"is:",x*y)
+print ("Division of",x,"&",y,"is:",x/y)
