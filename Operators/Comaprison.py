10e3b2484c8b640005146712d1ee6bd1363b8303
Deepesh Garg <63232108+Deepesh1502@users.noreply.github.com>
2022-02-01 21:05:21 +0530

    Create Comaprison.py

---
+++ Operators/Comaprison.py
@@ -0,0 +1,10 @@
+x = int(input("Enter a value: "))
+y = int(input("Enter a value: "))
+'''The values of comparison operators are
+either True or False'''
+print ('x > y is', x>y)
+print ('x < y is', x<y)
+print ('x == y is', x==y)
+print ('x != y is', x!=y)
+print ('x >= y is', x>=y)
+print ('x <= y is', x<=y)
