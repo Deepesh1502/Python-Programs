e1da5e0dd8209dbe3259b3cd4c09c3aa4203075e
Deepesh Garg <gargdeepesh2002@gmail.com>
2022-02-16 21:30:07 +0530

    Create WhileLoop.py

---
+++ Loops/WhileLoop.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number: "))
+d = int(input("Enter the difference: "))
+#The while loop will iterate until the condition gets false
+  while n>0:
+    print(n)
+#decrementing the value to exit from the loop
+    n=n-1
